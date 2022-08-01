c34a9031a89832fe987f56c7cde2e76877ec7366
enamul-java <enamul.java@gmail.com>
2022-08-01 13:14:09 +0600

    Initial commit

---
+++ app/src/main/java/com/ehaquesoft/madskillandroid/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ehaquesoft.madskillandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ehaquesoft.madskillandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
